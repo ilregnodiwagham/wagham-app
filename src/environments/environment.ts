ae1b45f71735d12288acb813541dd148f452e573
Vincenzo Claudio Pierro <v.c.pierro@gmail.com>
2022-10-29 14:20:04 +0200

    Updated deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,7 @@
 export const environment = {
   production: false,
   waghamApi: 'https://app.ilregnodiwagham.it/api',
+  guildId: '699173030722535474',
   // eslint-disable-next-line max-len
   discordAuthUrl: 'https://discord.com/api/oauth2/authorize?client_id=867838410064658481&redirect_uri=http%3A%2F%2Flocalhost%3A8100%2Ftabs%2Fhome&response_type=code&scope=identify%20guilds.members.read',
   waghamInviteUrl: 'https://discord.gg/Rgd6cnbupH'
